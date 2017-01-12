48eeac2f86cc394c4da0df8d6d8d1500dc366546
Vinay <vinayvishal@gmail.com>
2017-01-12 12:30:47 +0530

    changes with which injection exception gets reproduced

--- osgi-client/src/main/java/HelloWorld.java
+++ osgi-client/src/main/java/HelloWorld.java
@@ -1,16 +1,23 @@
 import java.io.IOException;
 import java.io.PrintWriter;
-import javax.servlet.annotation.WebServlet; 
-import javax.servlet.*;
-import javax.servlet.http.*;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.annotation.Resource;
+
+import org.glassfish.osgicdi.OSGiService;
+import javax.inject.Inject;
 
 import com.example.api.SimpleService;
 
 @WebServlet(urlPatterns={"/hello"})
 public class HelloWorld extends HttpServlet {
 
-    @Resource(mappedName="decl-service-1")
+    @Resource(mappedName="decl-service-2")
+//	@Inject @OSGiService(dynamic=true)
     SimpleService simpleService;
   
     public void doGet(HttpServletRequest req, HttpServletResponse res)
